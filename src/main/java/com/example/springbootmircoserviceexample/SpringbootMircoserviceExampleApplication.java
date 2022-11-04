4a933b779cca9a0ec36cdf98d7d9735b235ca9e1
Martin Choi <marin_choi@mtelnet.com>
2022-11-04 14:56:48 +0800

    commit and see the username and useremail

--- src/main/java/com/example/springbootmircoserviceexample/SpringbootMircoserviceExampleApplication.java
+++ src/main/java/com/example/springbootmircoserviceexample/SpringbootMircoserviceExampleApplication.java
@@ -16,6 +16,6 @@ public class SpringbootMircoserviceExampleApplication {
 
 	@GetMapping("/hello")
 	public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
+		return String.format("Hello %s!!", name);
 	}
 }
